68f1f3847fb03d23d08c381a3f6de06d92d19e13
fireflower0 <fireflower0@hotmail.co.jp>
2021-07-02 06:49:54 +0900

    Fixed a index in views

--- src/views/index.lisp
+++ src/views/index.lisp
@@ -10,13 +10,17 @@
   (with-html "Hello"
     (:h1 "Headline")
     (:div "Hello, world!")
+    (:br)
     (:h1 "Button")
     (:button "Clicked!")
+    (:br)
     (:h1 "Input")
     (:input)
+    (:br)
     (:h1 "List")
     (:ul (loop for v in '("foo" "bar" "baz")
                collect (markup (:li v))))
+    (:br)
     (:h1 "Table")
     (:table :border 1
      (loop for i from 1 to 9
